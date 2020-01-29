710b659346f81acf139dbb930bc148a4b23bef14
Shivam <shivamsharma0501@gmail.com>
2020-01-29 16:21:33 +0530

    implement DRY principle

--- Web Development/JavaScript- Pig Game/app.js
+++ Web Development/JavaScript- Pig Game/app.js
@@ -35,6 +35,29 @@ document.querySelector('.btn-roll').addEventListener('click',function() {
      diceDOM.src = 'dice-' + dice + '.png';
 
      //3. Update the round score if the rolled number was not a 1.
+        if(dice!==1)
+        {
+          //Add score
+          roundScore += dice;
+          document.querySelector('#current-' +activePlayer).textContent = roundScore;
+        }
+        else{
+          //next player
+          nextPlayer();
 
-
+        }
 });
+
+
+function nextPlayer(){
+  //next player
+  activePlayer === 0 ? activePlayer = 1: activePlayer = 0;
+  roundScore = 0;
+  document.getElementById('#current-0').textContent = '0';
+  document.getElementById('#current-1').textContent = '1';
+
+  document.querySelector('.player-0-panel').classList.toogle('active');
+  document.querySelector('.player-1-panel').classList.toogle('active');
+
+  document.querySelector('.dice').style.display = 'none';
+}
