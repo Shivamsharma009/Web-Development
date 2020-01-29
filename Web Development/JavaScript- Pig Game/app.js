e920f44429bc668b964755b9f40869319d787c4f
Shivam <shivamsharma0501@gmail.com>
2020-01-29 11:50:44 +0530

    fixed errors

--- Web Development/JavaScript- Pig Game/app.js
+++ Web Development/JavaScript- Pig Game/app.js
@@ -11,7 +11,7 @@ GAME RULES:
 var scores , roundScore, activePlayer;
 scores = [0, 0];
 roundScore = 0;
-activePlayer = 1  ;
+activePlayer = 1;
 //console.log(dice);
 
 //document.querySelector('#current-' + activePlayer).textContent = dice;
@@ -19,12 +19,12 @@ activePlayer = 1  ;
 
 document.querySelector('.dice').style.display = 'none';
 
-document.getElementbyId('score-0').textContent = '0';
-document.getElementbyId('score-1').textContent = '0';
-document.getElementbyId('current-0').textContent = '0';
-document.getElementbyId('current-1').textContent = '0';
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
 
-document.querySelector('.btn-roll').addEventListener('click',function(){
+document.querySelector('.btn-roll').addEventListener('click',function() {
 
    //1.Random number
     var dice = Math.floor(Math.random() * 6 ) + 1;
@@ -32,9 +32,9 @@ document.querySelector('.btn-roll').addEventListener('click',function(){
     //2. Display the result
      var diceDOM = document.querySelector('.dice');
      diceDOM.style.display = 'block';
-     diceDOM.src = 'dice' + dice + '.png';
+     diceDOM.src = 'dice-' + dice + '.png';
 
      //3. Update the round score if the rolled number was not a 1.
 
 
-})
+});
