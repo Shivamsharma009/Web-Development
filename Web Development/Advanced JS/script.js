ae8f6f47f7a03d72fd41c7d962c7fdf281b882bf
Shivam <shivamsharma0501@gmail.com>
2020-01-30 08:11:59 +0530

    start Primitive vs objects

--- Web Development/Advanced JS/script.js
+++ Web Development/Advanced JS/script.js
@@ -1,11 +1,12 @@
 //Function constructor and Inheritance
+/*
 var john = {
   name: 'John',
   yearofBirth:1990,
   job:'teacher'
 };
 
-//Best way to create Objects
+//to create Objects
 var Person = function(name,yearofBirth,job){
   this.name = name;
   this.yearofBirth = yearofBirth;
@@ -31,3 +32,28 @@ mark.calculateAge();
 console.log(john.lastName);
 console.log(jane.lastName);
 console.log(mark.lastName);
+*/
+//Another way to create Objects in that case the function inherent the property which
+//i mention in the prototype.
+/*
+var personProto = {
+  calculateAge:function(){
+    console.log(2016 - this.yearofBirth);
+  }
+};
+
+var john = Object.create(personProto);
+john.name = 'John';
+john.yearofBirth = 1990;
+john.job = 'teacher';
+
+
+var jane = Object.create(personProto,
+{
+  name :{value : 'jane'},
+  yearofBirth : { value :1969},
+  job: { value : 'designer'}
+});
+*/
+
+//Primitive vs Objects
