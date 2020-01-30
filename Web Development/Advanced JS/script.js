5b171ed77318d6dc66b4685611a9b4302ab1fae7
Shivam <shivamsharma0501@gmail.com>
2020-01-30 14:44:42 +0530

    hmmmm

--- Web Development/Advanced JS/script.js
+++ Web Development/Advanced JS/script.js
@@ -96,3 +96,38 @@ console.log(obj.city);
 */
 ///////////////////
 ////////Passing Function as argument in js
+var years = [1990,1965,1937,2005,1998];
+
+function arrayCalc(arr,fn){
+  var arrRes = [];
+  for(var i = 0;i < arr.length;i++){
+    arrRes.push(fn(arr[i]));
+  }
+  return arrRes;
+}
+
+function calculateAge(el){
+  return 2016 - el;
+}
+var ages = arrayCalc(years,calculateAge);
+console.log(ages);
+
+//Function Returning Function
+function interviewQuestion(job){
+  if(job === 'designer'){
+    return function(name) {
+      console.log(name + ',can you please explain what ux design is ?');
+    }
+  }else if(job === 'teacher'){
+    return function(name){
+      console.log('What subject do you teach ,' + name + '?');
+    }
+  }else {
+    return function(name) {
+      console.log('Hello' + name + ',what do you do ?');
+    }
+  }
+}
+
+var teacherQuestion = interviewQuestion('teacher');
+teacherQuestion('John');
